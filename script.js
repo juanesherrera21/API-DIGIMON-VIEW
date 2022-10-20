fbfea9d9e546bdf2d73fe6af208990a2a0ddd6e9
Juan Esteban Herrera Tapia <68380237+juanesherrera21@users.noreply.github.com>
2022-10-19 22:06:50 -0500

    Termino el  proyecto
    subo la conexion a la API y sus elementos, adunto vistas etc etc

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function actionButtonCredit() {
 
 ConexionAPI();
 
+var VectorNombres = [];
+var Nombre = "";
+var lvl = "";
 
 function ConexionAPI() {
     let url = 'https://digimon-api.vercel.app/api/digimon';
@@ -24,24 +27,84 @@ function ConexionAPI() {
         .catch(error => console.log(error))
 
     var mostrarData = (data) => {
-        console.log(data)   //Informacion del Json
+        //console.log(data)   //Informacion del Json
         let body = ""
-        for (let index = 0; index < data.length; index++) {
+        for (let index = 0; index < 50; index++) {
             //console.log(data[index]);
            // VectorNombres.push(data[index]);
-
-            console.log(data[index]);
-            console.log("name:"+data[index].name);
-            console.log("lvl:"+data[index].level);
+            VectorNombres.push(data[index].name);
+            
+           // console.log("name:"+data[index].name);
+           // console.log("lvl:"+data[index].level);
 
         }
+        TraerDatosPj();
 
         
-
-
-
         //console.log(body)
     }
+
+}
+
+function TraerDatosPj() {
+
+    for (let index = 0; index < 50; index++) {
+        
+        var url = `https://digimon-api.vercel.app/api/digimon/name/${VectorNombres[index]}`;
+        
+        //image = `https://api.genshin.dev/characters/${VectorNombres[index]}/icon.png`
+        
+        fetch(url)
+            .then(response => response.json())
+            .then(data => mostrarData(data))
+            .catch(error => console.log(error))
+
+        var mostrarData = (data) => {
+            
+            console.log(data);
+            Nombre = String(data[0].name);
+            console.log(String(data[0].name));
+            lvl = data[0].level;
+            image=data[0].img;  
+            
+
+
+
+            createCard(Nombre,lvl,image);
+
+
+            //console.log(body)
+        }
+    }
+
+
+
+}
+
+
+function createCard(texto1, texto2, texto3) {
+
+    
+
+
+    let card = document.getElementById("row row-cols-1 row-cols-md-2");
+
+
+    card.innerHTML += `   <div class="col mb-4">
+        <div class="card" id="card">
+            <div class="card-body" id="cardbody">
+                <img src="${texto3}" alt="Avatar" style="width:150px; border-radius: 50%;">
+                <h5 class="card-title" style="color: white;">Nombre: ${texto1}</h5> 
+                <p class="card-text" style="color: white;">${texto2}.</p>
+                
+                
+            </div>
+    
+        </div>
+    </div>
+    `;
+
+
 
 }
 
